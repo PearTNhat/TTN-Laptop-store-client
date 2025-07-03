3663756157f451a2879d09b9e78d4f082d2bdfc1
nguyendinhluat <nguyendinhluat18032k3@gmail.com>
2025-07-03 23:09:53 +0700

    luat: Login/Register/Reset-password

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import PublicLayout from "./pages/public/PublicLayout";
 import Home from "./pages/public/home/Home";
 import Login from "./pages/public/auth/Login";
 import Register from "./pages/public/auth/Register";
+import ResetPassword from "./pages/public/auth/ResetPassword";
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
           <Route element={<Home />} />
           <Route path={publicPaths.LOGIN} element={<Login />} />
           <Route path={publicPaths.REGISTER} element={<Register />} />
+          <Route path={publicPaths.RESET_PASSWORD} element={<ResetPassword />} />
         </Route>
       </Routes>
     </div>
